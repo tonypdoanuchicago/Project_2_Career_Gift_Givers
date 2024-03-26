d56c0b7529b5dc78927bd9507e5308246e3598e4
mmiller1234321 <148365315+mmiller1234321@users.noreply.github.com>
2024-03-26 08:51:36 -0500

    Hello World message returned locally

--- config/connection.js
+++ config/connection.js
@@ -4,13 +4,13 @@ require('dotenv').config();
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'postgres',
-    }
-  );
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST || 'localhost',
+        dialect: process.env.DB_DIALECT || 'postgres',
+      }
+    );
 
 module.exports = sequelize;
