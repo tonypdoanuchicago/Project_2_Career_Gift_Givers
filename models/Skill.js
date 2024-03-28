3d2f6003a1d8b592f024aeb95be5bf7de0da1c5f
mmiller1234321 <148365315+mmiller1234321@users.noreply.github.com>
2024-03-27 22:58:59 -0500

    updated models

--- models/Skill.js
+++ models/Skill.js
@@ -1,26 +1,12 @@
-const { Model, DataTypes } = require('sequelize');
+const { DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Skill extends Model {}
-
-Skill.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    }
+const Skill = sequelize.define('Skill', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
   },
-  {
-    sequelize,
-    timestamps: false,
-    modelName: 'Skill'
-  }
-);
+});
 
 module.exports = Skill;
--- models/tutor.js
+++ models/tutor.js
@@ -1,53 +1,20 @@
-const { Model, DataTypes } = require('sequelize');
+const { DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-const Skill = require('./Skill');
 
-class Tutor extends Model {}
-
-Tutor.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notNull: true,
-        notEmpty: true,
-        len: [2, 50]
-      }
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        notNull: true,
-        notEmpty: true,
-        isEmail: true
-      }
-    },
-    job_title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notNull: true,
-        notEmpty: true,
-        len: [2, 100]
-      }
-    }
+const Tutor = sequelize.define('Tutor', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
   },
-  {
-    sequelize,
-    timestamps: true,
-    modelName: 'Tutor'
-  }
-);
-
-// Define association between Tutor and Skill
-Tutor.belongsToMany(Skill, { through: 'TutorSkill' });
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  job_title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
 
 module.exports = Tutor;
