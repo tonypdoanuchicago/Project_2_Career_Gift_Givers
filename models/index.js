d1c7e9c4e8e7fa42777eb29ebabf52c2871400e7
mmiller1234321 <148365315+mmiller1234321@users.noreply.github.com>
2024-03-27 22:39:29 -0500

    update files to match 14-28

--- models/index.js
+++ models/index.js
@@ -1,13 +1,4 @@
 const Tutor = require('./Tutor');
 const Skill = require('./Skill');
 
-Tutor.hasMany(Skill, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
-
-Skill.belongsTo(Tutor, {
-  foreignKey: 'user_id'
-});
-
-module.exports = { User, Project };
+module.exports = { Tutor, Skill };
